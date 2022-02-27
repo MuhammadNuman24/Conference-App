7981c93488b2f2eac1bccdfaa02021848dd3c070
muhammadnuman24 <numanrazzaq24@gmail.com>
2022-02-27 11:44:28 +0500

    set data bean in configuration file

--- src/main/java/com/example/crypto/config/PersistenceConfiguration.java
+++ src/main/java/com/example/crypto/config/PersistenceConfiguration.java
@@ -1,18 +1,18 @@
 package com.example.crypto.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/crypto");
-        System.out.println("My custom source bean has been initialized and set.");
-        return builder.build();
-    };
+    // @Bean
+    // public DataSource dataSource() {
+    //     DataSourceBuilder builder = DataSourceBuilder.create();
+    //     builder.url("jdbc:postgresql://localhost:5432/crypto");
+    //     System.out.println("My custom source bean has been initialized and set.");
+    //     return builder.build();
+    // };
 }
